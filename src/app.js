f77fac1b27f17b84746ff45589d98c2531db4f3e
vinfry <mercurial.sp@gmail.com>
2019-04-04 11:56:45 +0300

    added streaming

--- src/app.js
+++ src/app.js
@@ -1,33 +1,51 @@
 var _msbuild = require('msbuild');
-var msbuild = new _msbuild();
 
 function build(action) {
     return new Promise((resolve,reject) => {
+        var msbuild = new _msbuild();
         let path = action.params.PATH;
         let config = action.params.CONFIG;
         msbuild.configuration = config;
         msbuild.sourcePath = path;
-        msbuild.build(function (err, res) {
-            if (err) {
-                return reject (err);
-            }
-            resolve (res)
+
+        msbuild.on('error',function(err,results){
+            reject ({err : err, status:"failure"})
         });
+
+        msbuild.on('done',function(err,results){
+            if (err) return reject ({err : err, status:"failure"});
+
+            resolve ({
+                status : "success"
+            })
+        });
+
+        msbuild.build();
     })
 }
 
 function publish(action) {
     return new Promise((resolve,reject) => {
+        var msbuild = new _msbuild();
         let path = action.params.PATHPROJ;
         let profile = action.params.PROFILE;
         msbuild.publishProfile = profile;
         msbuild.sourcePath = path;
-        msbuild.publish(function (err, res) {
-            if (err) {
-                return reject (err);
-            }
-            resolve (res)
+
+
+        msbuild.on('error',function(err,results){
+            reject ({err : err, status:"failure"})
         });
+
+        msbuild.on('done',function(err,results){
+            if (err) return reject ({err : err, status:"failure"});
+
+            resolve ({
+                status : "success"
+            })
+        });
+
+        msbuild.publish();
     })
 }
 
